9dd6b4550969bacca8cf5c26bbea298efbd7cfb4
jweezy <jwest1@luc.edu>
2019-01-27 20:30:15 -0600

    able to read zettels into a spreadsheet.

--- grab&uploadZets.py
+++ grab&uploadZets.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 import pickle
-import os.path
+import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/script.projects']
@@ -12,29 +13,6 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapi
 SAMPLE_SPREADSHEET_ID = '1gcHzsXx6Vmhg5lxTeDKFFQL6WZveI3AvAr_dBW43FJI'
 SAMPLE_RANGE_NAME = 'A2:H85'
 
-#google script code
-create_doc = '''
-function createDoc() {
-  doc = DocumentApp.create('Document Name');
-}
-'''.strip()
-
-get_range_of_doc = '''
-function createDoc() {
-  var doc = SpreadsheetApp.openById('1gcHzsXx6Vmhg5lxTeDKFFQL6WZveI3AvAr_dBW43FJI');
-  var namedRanges = doc.getNamedRanges()
-  if (namedRanges.length > 1) {
-  Logger.log(namedRanges[0].getName());
-}
-}
-'''.strip()
-
-SAMPLE_MANIFEST = '''
-{
-  "timeZone": "America/New_York",
-  "exceptionLogging": "CLOUD"
-}
-'''.strip()
 
 def main():
     """Shows basic usage of the Sheets API.
@@ -59,38 +37,107 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    #service = build('sheets', 'v4', credentials=creds)
+    service = build('sheets', 'v4', credentials=creds)
     service2 = build('script', 'v1', credentials=creds)
 
     # Call the Sheets API
-    #sheet = service.spreadsheets()
-    #result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-    #                            range=SAMPLE_RANGE_NAME).execute()
-    #values = result.get('values', [])
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                               range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+    info = []
+    path = "/home/jweezy/Documents/Code/zettelgeist/docs/example/mlb"
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for f in filenames:
+            if ".yaml" in f:
+                info.append(parseFile(path + "/"+ f))
+
+    request3 = {
+    "function": "update",
+    "devMode": True,
+    "parameters": ["1cFPIXaPGawk1ULZj2NnSKCzOHmLatBMc04vSVZ-czJE"]
+    }
+
+    titles = []
+    row = 2
+    for i in info:
+        for j in i.keys():
+            if j == "title":
+                request3.update({"parameters": request3.get("parameters") + ["A"+str(row)]})
+                request3.update({"parameters": request3.get("parameters") + [i.get(j)]})
+                response = service2.scripts().run(scriptId='AKfycbxrE7z65fIIxxxmtXoV8tcZu3v5GTu4Hc053BnL0rKVrJ4fHoE', body=request3).execute()
+
+            if j == "url":
+                request3.update({"parameters": request3.get("parameters")+ ["B"+str(row)]})
+                request3.update({"parameters": request3.get("parameters")+ [i.get(j)]})
+                response = service2.scripts().run(scriptId='AKfycbxrE7z65fIIxxxmtXoV8tcZu3v5GTu4Hc053BnL0rKVrJ4fHoE', body=request3).execute()
+
+            if j == "summary":
+                request3.update({"parameters": request3.get("parameters")+ ["C"+str(row)]})
+                request3.update({"parameters": request3.get("parameters")+ [i.get(j)]})
+                response = service2.scripts().run(scriptId='AKfycbxrE7z65fIIxxxmtXoV8tcZu3v5GTu4Hc053BnL0rKVrJ4fHoE', body=request3).execute()
+
+            if j == "note":
+                request3.update({"parameters": request3.get("parameters")+ ["D"+str(row)]})
+                request3.update({"parameters": request3.get("parameters")+ [i.get(j)]})
+                response = service2.scripts().run(scriptId='AKfycbxrE7z65fIIxxxmtXoV8tcZu3v5GTu4Hc053BnL0rKVrJ4fHoE', body=request3).execute()
+
+            if j == "tags":
+                request3.update({"parameters": request3.get("parameters")+ ["E"+str(row)]})
+                request3.update({"parameters": request3.get("parameters")+ [i.get(j)]})
+                response = service2.scripts().run(scriptId='AKfycbxrE7z65fIIxxxmtXoV8tcZu3v5GTu4Hc053BnL0rKVrJ4fHoE', body=request3).execute()
+
+            request3 = {
+            "function": "update",
+            "devMode": True,
+            "parameters": ["1cFPIXaPGawk1ULZj2NnSKCzOHmLatBMc04vSVZ-czJE"]
+            }
+        row += 1
+
+
+
 
 
     #try:
-    request = { # A request to run the function in a script. The script is identified by the
-      # specified `script_id`. Executing a function on a script returns results
-      # based on the implementation of the script.
-    "function": "createDoc", # The name of the function to execute in the given script. The name does not
-        # include parentheses or parameters.
-    "devMode": True, # If `true` and the user is an owner of the script, the script runs at the
-        # most recently saved version rather than the version deployed for use with
-        # the Apps Script API. Optional; default is `false`.
-    "parameters": [ # The parameters to be passed to the function being executed. The object type
-        # for each parameter should match the expected type in Apps Script.
-        # Parameters cannot be Apps Script-specific object types (such as a
-        # `Document` or a `Calendar`); they can only be primitive types such as
-        # `string`, `number`, `array`, `object`, or `boolean`. Optional.
-      "",
+    request = {
+    "function": "getRange",
+    "devMode": True,
+    "parameters": [
+    "1rqFwTIINwswir1uCB7q21_m4reyyT76uUJOUbKaPwbE",
     ]
-  }
-    response = service2.scripts().run(scriptId='AKfycbxrE7z65fIIxxxmtXoV8tcZu3v5GTu4Hc053BnL0rKVrJ4fHoE', body=request).execute()
+    }
+    request2 = {
+    "function": "createDoc",
+    "devMode": True,
+    "parameters": ["testZetel"]
+    }
+
     folderSet = response['response'].get('result')
     print(folderSet)
     #except:
         # The API encountered a problem.
         #print("There was a problem homie.")
+def parseFile(file):
+    info = open(file, "r")
+    files = {file: ''}
+    dict = {"title":'', "summary": "", "url":'', "note":"", "tags":"" }
+    for i in info:
+        for j in dict.keys():
+            if j+":" in i:
+                currentTag = j
+                if j == "url":
+                    firstLine = i.split("url:")[1]
+                    break
+                firstLine = i.split(":")[1]
+                break
+        if firstLine != '':
+            dict.update({currentTag: firstLine})
+            firstLine = ''
+        else:
+            dict.update({currentTag: dict.get(currentTag)+ "\n" + i})
+    info.close()
+    return dict
+
+
 
 main()
